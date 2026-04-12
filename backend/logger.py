4b237008edf5bdb18247b58581f55057c3691122
Lucy <aliraza.bot241@gmail.com>
2026-04-12 21:26:07 +0500

    update logging code in db.py

---
+++ backend/logger.py
@@ -0,0 +1,28 @@
+import logging
+from pathlib import Path
+
+log_dir = Path(__file__).parent.parent / "logs"
+
+# db specific log
+db_handler = logging.FileHandler(log_dir / "db.log")
+
+# errors only
+error_handler = logging.FileHandler(log_dir / "error.log")
+error_handler.setLevel(logging.ERROR)  # only ERROR and CRITICAL
+
+# sends logs in real time
+stream_handler = logging.StreamHandler()
+
+# configures logs settings
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        db_handler,
+        error_handler,
+        stream_handler
+    ]
+)
+
+def get_logger(name: str):
+    return logging.getLogger(name)
