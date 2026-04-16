4a5cb5c4b70542705ffca3101b0beeb5ff767b65
Lucy <aliraza.bot241@gmail.com>
2026-04-16 12:14:25 +0500

    initialize fastAPI app

--- backend/main.py
+++ backend/main.py
@@ -1,75 +1,16 @@
-from fastapi import FastAPI, HTTPException, status, Path
-from typing import Optional
-from pydantic import BaseModel
+""" Initialized FastAPI app and includes all routers"""
 
-app = FastAPI()
+from fastapi import FastAPI
+from routers import users, clients, organizations, roles, organization_members, revenue, expenses, risk_alerts, financial_snapshots
 
-users = {1: {"name": "Ali", "website": "www.hi.com", "age": 22, "role": "developer"}}
+app = FastAPI(title="RISCO DBMS")
 
-
-# Base Pydantic Models
-class User(BaseModel):
-    name: str
-    website: str
-    age: int
-    role: str
-
-
-class UpdateUser(BaseModel):
-    name: Optional[str] = None
-    website: Optional[str] = None
-    age: Optional[int] = None
-    role: Optional[str] = None
-
-
-# Endpoint
-@app.get("/")
-def root():
-    return {"message": "Welcome"}
-
-
-# Get Users
-@app.get("/users/{user_id}")
-def get_user(user_id: int = Path(..., description="The ID you want to get", gt=0)):
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail="User Not Found!")
-    return users[user_id]
-
-
-# Create a User
-@app.post("/users/{user_id}", status_code=status.HTTP_201_CREATED)
-def create_user(user_id: int, user: User):
-    if user_id in users:
-        raise HTTPException(status_code=400, detail="User already exists")
-    users[user_id] = user.dict()
-    return user
-
-
-# Update a User
-@app.put("/users/{user_id}")
-def update_user(user_id: int, user: UpdateUser):
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail="User Not Found!")
-    current_user = users[user_id]
-    if user.name is not None:
-        current_user["name"] = user.name
-    if user.website is not None:
-        current_user["website"] = user.website
-    if user.age is not None:
-        current_user["age"] = user.age
-    if user.role is not None:
-        current_user["role"] = user.role
-    return current_user
-
-
-# Delete a User
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail="User Not Found!")
-
-    deleted_user = users.pop(user_id)
-    return {"message": "User has been deleted", "deleted user:": deleted_user}
-
-
-# Search for a user
+app.include_router(users.router)
+app.include_router(clients.router)
+app.include_router(organizations.router)
+app.include_router(roles.router)
+app.include_router(organization_members.router)
+app.include_router(revenue.router)
+app.include_router(expenses.router)
+app.include_router(risk_alerts.router)
+app.include_router(financial_snapshots.router)
