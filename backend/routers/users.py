546bc3352eff082bc26c6c487aa7284664536b4a
Lucy <aliraza.bot241@gmail.com>
2026-04-16 16:24:02 +0500

    setup frontend directory structure

---
+++ backend/routers/users.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from db import get_db
+import schema, models 
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+#@router.get
